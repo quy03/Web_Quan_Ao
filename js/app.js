17bb36bf0e1f74fb30557c4b32b95f331ad3dca3
quy03 <quytrinh439@gmail.com>
2023-11-14 11:13:06 +0700

    upload source code

--- js/app.js
+++ js/app.js
@@ -20,54 +20,52 @@ window.onscroll = function() {
 
 /*--------------------------Icon--------------------------- */
 
-function showSearchBox() {
-    var searchBox = document.getElementById("searchBox");
-    var loginBox = document.getElementById("loginBox");
-    var cartBox = document.getElementById("cartBox");
+// function showSearchBox() {
+//     var searchBox = document.getElementById("searchBox");
+//     var loginBox = document.getElementById("loginBox");
+//     var cartBox = document.getElementById("cartBox");
 
-    if (searchBox.style.display === "none" || searchBox.style.display === "") {
-        loginBox.style.display = "none";
-        cartBox.style.display = "none";
-        searchBox.style.display = "block";
-    } else {
-        searchBox.style.display = "none";
-    }
+//     if (searchBox.style.display === "none" || searchBox.style.display === "") {
+//         loginBox.style.display = "none";
+//         cartBox.style.display = "none";
+//         searchBox.style.display = "block";
+//     } else {
+//         searchBox.style.display = "none";
+//     }
 
-}
+// }
 
-function showLoginBox() {
-    var searchBox = document.getElementById("searchBox");
-    var loginBox = document.getElementById("loginBox");
-    var cartBox = document.getElementById("cartBox");
+// function showLoginBox() {
+//     var searchBox = document.getElementById("searchBox");
+//     var loginBox = document.getElementById("loginBox");
+//     var cartBox = document.getElementById("cartBox");
 
-    if (loginBox.style.display === "none" || loginBox.style.display === "") {
-        searchBox.style.display = "none";
-        cartBox.style.display = "none";
-        loginBox.style.display = "block";
-    } else {
-        loginBox.style.display = "none";
-    }
+//     if (loginBox.style.display === "none" || loginBox.style.display === "") {
+//         searchBox.style.display = "none";
+//         cartBox.style.display = "none";
+//         loginBox.style.display = "block";
+//     } else {
+//         loginBox.style.display = "none";
+//     }
 
-}
+// }
 
-function showCartBox() {
-    var searchBox = document.getElementById("searchBox");
-    var loginBox = document.getElementById("loginBox");
-    var cartBox = document.getElementById("cartBox");
+// function showCartBox() {
+//     var searchBox = document.getElementById("searchBox");
+//     var loginBox = document.getElementById("loginBox");
+//     var cartBox = document.getElementById("cartBox");
 
-    if (cartBox.style.display === "none" || cartBox.style.display === "") {
-        searchBox.style.display = "none";
-        loginBox.style.display = "none";
-        cartBox.style.display = "block";
-    } else {
-        cartBox.style.display = "none";
-    }
-}
-
-
+//     if (cartBox.style.display === "none" || cartBox.style.display === "") {
+//         searchBox.style.display = "none";
+//         loginBox.style.display = "none";
+//         cartBox.style.display = "block";
+//     } else {
+//         cartBox.style.display = "none";
+//     }
+// }
 
 /*------------------------Kế thừa --------------------------- */
 // JavaScript để xử lý sự kiện click trên menu
-document.getElementById('fixed-menu').addEventListener('click', function() {
-    window.location.href = 'new_collection.html';
-});
+// document.getElementById('fixed-menu').addEventListener('click', function() {
+//     window.location.href = 'new_collection.html';
+// });
